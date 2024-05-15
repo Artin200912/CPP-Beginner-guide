a3a395bbd4348187a0b81a1865fdef2e3c0ab994
Artin Daneshvar <artindaneshvar23@gmail.com>
2024-05-16 00:24:32 +0330

    First commit

---
+++ overloaded_functions.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+void BakePizza();
+
+int main(){
+
+    BakePizza();
+
+    return 0;
+}
+
+
+void BakePizza(){
+    cout << "Here is your pizza\n";
+}
