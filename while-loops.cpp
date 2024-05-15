8d531831dff0ee1933e185d2f6f8170813d92af5
Artin Daneshvar <artindaneshvar23@gmail.com>
2024-05-15 21:49:05 +0330

    first commit

---
+++ while-loops.cpp
@@ -0,0 +1,22 @@
+// while loops are useful when your trying to loop through something or you want to force the program to do something
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main()
+{
+
+    string name;
+
+    while (name.empty()){
+        cout << "Enter your name : ";;
+        std::getline(cin, name);
+    }
+    cout << "hello " << name;
+
+
+    return 0;
+}
+
